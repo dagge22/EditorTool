da3d6991b2018a768f2c09824fd53e18bcf0d25e
Dag <dag.w@hotmail.com>
2019-04-30 14:26:13 +0200

    Initial commit

---
+++ Assets/Circle.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public int vertexes = 40;
+    public float radius;
+
+    LineRenderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        CreateCircle();
+    }
+
+    void CreateCircle()
+    {
+        renderer.widthMultiplier = 0.1f;
+
+        float deltaTheta = (2f * Mathf.PI) / vertexes;
+        float theta = 0f;
+
+        renderer.positionCount = vertexes + 1;
+
+        for (int i = 0; i < renderer.positionCount; i++)
+        {
+            Vector3 pos = new Vector3(radius * Mathf.Cos(theta), radius * Mathf.Sin(theta), 0f);
+            renderer.SetPosition(i, pos);
+
+            theta += deltaTheta;
+        }
+    }
+}
---
+++ Assets/Editor/CircleEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CircleEditor : EditorWindow
+{
+    public Circle circle = null;
+    int ver = 3;
+    int rad = 3;
+
+    [MenuItem("Window/ Cirlce Editor")]
+
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(CircleEditor));
+    }
+
+    void OnGUI()
+    {
+        circle = (Circle)EditorGUILayout.ObjectField("Circle", circle, typeof(Circle), true);
+        if (circle != null)
+        {
+            ver = (int)EditorGUILayout.Slider("Vertexis", ver, 3, 500);
+            circle.vertexes = ver;
+
+            rad = (int)EditorGUILayout.Slider("Radius", rad, 0, 20);
+            circle.radius = rad;
+
+        }
+    }
+
+    void OnSCeneGUI()
+    {
+        //Circle t = (circle as Circle);
+        rad = (int)Handles.RadiusHandle(Quaternion.identity, circle.transform.position, circle.radius);
+        circle.radius = rad;
+    }
+}
